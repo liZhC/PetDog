34d9f5eb12aa5c3178108c999126c5405c38e04d
huangxiaoxu <huangxiaoxu93@gmail.com>
2016-04-27 20:30:12 +0800

    add demo code

---
+++ src/main/java/com/dog/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.dog.demo;
+
+/**
+ * Created by Huangxiaoxu on 2016/4/27.
+ */
+public class Demo {
+
+    public static void main(String[] args){
+        System.out.println("I am not Happy!");
+    }
+}
---
+++ src/test/java/CodeCheck.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Huangxiaoxu on 2016/4/27.
+ */
+public class CodeCheck {
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
